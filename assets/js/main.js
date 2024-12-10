a99f2eb20652e99e20511c12e4a84abad0290ec2
John L <john@johnle.org>
2024-12-10 06:21:03 -0500

    ---

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,53 +50,44 @@ const App = () => {
         setLoadingPosts(true);
         let fetchUrl;
         if (selectedSubreddit.startsWith('u/')) {
-            const username = selectedSubreddit.substring(2);
-            fetchUrl = `https://www.reddit.com/user/${username}/submitted/${sort}.json?count=${(page - 1) * 25}`;
+          const username = selectedSubreddit.substring(2);
+          fetchUrl = `https://www.reddit.com/user/${username}/submitted/${sort}.json?count=${(page - 1) * 25}`;
         } else {
-            fetchUrl = `https://www.reddit.com/${selectedSubreddit}/${sort}.json?count=${(page - 1) * 25}`;
+          fetchUrl = `https://www.reddit.com/${selectedSubreddit}/${sort}.json?count=${(page - 1) * 25}`;
         }
-    
+      
         fetch(fetchUrl)
-            .then(response => {
-                if (!response.ok) {
-                    setContentBlockerDetected(true);
-                    setErrorMessage(`Error: ${response.status} ${response.statusText}`);
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const fetchedPosts = data.data.children.map(child => ({
-                    id: child.data.id,
-                    title: child.data.title,
-                    author: child.data.author,
-                    content: child.data.selftext,
-                    url: child.data.url,
-                    subreddit: child.data.subreddit_name_prefixed,
-                    created_utc: child.data.created_utc,
-                    gallery_data: child.data.gallery_data,
-                    media_metadata: child.data.media_metadata,
-                    pinned: child.data.stickied,
-                    edited: child.data.edited,
-                    ups: child.data.ups - child.data.downs,
-                    media: child.data.media,
-                    flair: child.data.link_flair_text || '',
-                    nsfw: child.data.over_18
-                }));
-                
-                setPosts(prevPosts => [...fetchedPosts]);
-                setContentBlockerDetected(false);
-
-            })
-            .catch(error => {
-                console.error('Post fetch error:', error);
-                setContentBlockerDetected(true);
-            })
-            .finally(() => {
-                setLoadingPosts(false);
-            });
-    };
-
+          .then(response => response.json())
+          .then(data => {
+            const fetchedPosts = data.data.children.map(child => ({
+              id: child.data.id,
+              title: child.data.title,
+              author: child.data.author,
+              content: child.data.selftext,
+              url: child.data.url,
+              subreddit: child.data.subreddit_name_prefixed,
+              created_utc: child.data.created_utc,
+              gallery_data: child.data.gallery_data,
+              media_metadata: child.data.media_metadata,
+              pinned: child.data.stickied,
+              ups: child.data.ups - child.data.downs,
+              media: child.data.media,
+              flair: child.data.link_flair_text || '',
+              nsfw: child.data.over_18,
+              isEdited: child.data.edited
+            }));
+      
+            setPosts(prevPosts => [...prevPosts, ...fetchedPosts]);
+            setContentBlockerDetected(false);
+          })
+          .catch(error => {
+            console.error('Post fetch error:', error);
+            setContentBlockerDetected(true);
+          })
+          .finally(() => {
+            setLoadingPosts(false);
+          });
+      }
     const fetchComments = (postId) => {
         setLoadingComments(true);
         const post = posts.find(p => p.id === postId) || savedPosts.find(p => p.id === postId);
@@ -559,6 +550,7 @@ const App = () => {
                         </div>
                         <div className="text-gray-400 ml-4 flex-shrink-0">
                             <span className="flex items-center">
+                                {console.log(selectedPost.isEdited)}
                                 <i className="fas fa-arrow-up mr-1"></i>
                                 {formatUpvotes(selectedPost.ups)}
                             </span>
@@ -1137,7 +1129,7 @@ const App = () => {
                     </div>
                     <div className="text-gray-400 ml-4 flex-shrink-0">
                         <span className="flex items-center">
-                            {post.edited && <i className="fas fa-pencil-alt text-gray-400" title="Edited"></i>}
+                            {post.edited && <i className="fas fa-pencil-alt text-gray-400 ml-2" title="Edited"></i>}
                             <i className="fas fa-arrow-up mr-1"></i>
                             {formatUpvotes(post.ups)}
                         </span>
